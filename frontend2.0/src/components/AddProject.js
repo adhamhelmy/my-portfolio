93bea7ab7c163967bd7c61ce7bc13060d196bc78
adhamhelmy <adhamhelmy68@icloud.com>
2022-09-17 04:02:38 +0200

    last

--- frontend2.0/src/components/AddProject.js
+++ frontend2.0/src/components/AddProject.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {React, useState} from "react";
 import {Switch, Route, Link} from "react-router-dom"
-// import "bootstrap/dist/css/bootstrap.min.css"
+//import "bootstrap/dist/css/bootstrap.min.css"
 import AdminNavbar from './AdminNavbar';
 import ProjectData from '../Services/Project';
 const AddProject = props => {
