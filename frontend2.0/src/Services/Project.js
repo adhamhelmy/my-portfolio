ccdb1a2b1cd11fcf46edc547d5688cbe05753874
adhamhelmy <adhamhelmy68@icloud.com>
2022-09-20 21:09:11 +0200

    routing fix

--- frontend2.0/src/Services/Project.js
+++ frontend2.0/src/Services/Project.js
@@ -3,16 +3,16 @@ import http from "../http-common";
 
 class ProjectData {
     getAll() {
-        return http.get('http://localhost:5000/api/v1/projects');
+        return http.get('/api/v1/projects');
     }
 
     createProject(data) {
-        return http.post("http://localhost:5000/api/v1/projects/add/", data);
+        return http.post("/api/v1/projects/add/", data);
     }
 
     deleteProject(data) {
         console.log(data._id)
-        return http.delete("http://localhost:5000/api/v1/projects/delete?id="+data._id, data);
+        return http.delete("/api/v1/projects/delete?id="+data._id, data);
     }
 }
 
