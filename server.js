6f401bc4a9ed870d13da90354ea5b6ab7b69261e
adhamhelmy <adhamhelmy68@icloud.com>
2022-09-23 03:13:33 +0200

    adminpage

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ if(process.env.NODE_ENV=== 'production') {
 // code above was addd for deployment
 app.use(express.static(__dirname + '/public'))
 app.get('*', function (request, response){
-    response.sendFile(path.join(__dirname, 'frontend2.0/public/index.html'))
+    res.json('we got here')
+    // response.sendFile(path.join(__dirname, 'frontend2.0/public/index.html'))
   })
 
 
